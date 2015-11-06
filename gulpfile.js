f208a48f144439ab065f643a1f9fd271e2f7534d
tomdavenport <playmusic@me.com>
2015-11-06 11:56:13 +0000

    Experiment with homepage title size

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,8 @@ var Paths = {
   HERE                 : './',
   DIST                 : 'dist',
   DIST_TOOLKIT_JS      : 'dist/toolkit.js',
-  LESS_TOOLKIT_SOURCES : './less/toolkit*',
-  LESS                 : './less/**/**',
+  LESS_TOOLKIT_SOURCES : './_less/toolkit*',
+  LESS                 : './_less/**/**',
   JS                   : [
       './js/bootstrap/transition.js',
       './js/bootstrap/alert.js',
